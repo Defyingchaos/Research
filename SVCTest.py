6cb6a6d142f82527a02afd32b02244a74ef13521
Josh Levy <jdlrm77@gmail.com>
2018-01-31 14:41:07 -0600

    1/31/2018
    Implemented test_train_split

--- SVCTest.py
+++ SVCTest.py
@@ -4,9 +4,11 @@ import sklearn
 from sklearn.svm import SVC
 from sklearn.svm import NuSVC
 from sklearn.metrics import accuracy_score, average_precision_score, f1_score
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from numpy import genfromtxt
+
 
 #importing files to arrays
 CD = genfromtxt('C:\\Users\Research\Research Data\SSCurveData.csv', delimiter=',')
@@ -15,32 +17,38 @@ TCD = genfromtxt('C:\\Users\Research\Research Data\SSCurveDataTest.csv', delimit
 TCL = genfromtxt('C:\\Users\Research\Research Data\SSLablesTest.csv', delimiter=',')
 
 
-#scaling data
+#scaling and splitting data
 CD_scaled = preprocessing.scale(CD)
 TCD_scaled = preprocessing.scale(TCD)
 
+X_train, X_test, Y_train, Y_test = train_test_split(CD, CL)
+
 #Creating and training model
 model = SVC(C= 1.0, kernel='poly', degree = 2)
-model.fit(CD, CL)
+model.fit(X_train, Y_train)
 
 
-score()
+
 #scoring model
 def score():
-    score = model.score(TCD, TCL)
+    predicted=model.predict(X_test)
+    score = model.score(X_test, Y_test)
     print("normal score")
     print(score)
+
     print("accuracy_score")
-    predicted=model.predict(TCD)
-    jss=accuracy_score(TCL, predicted)
-    print(jss)
+    accscore=accuracy_score(Y_test, predicted)
+    print(accscore)
+
     print("f1 score")
-    f1=f1_score(TCL, predicted, average='binary')
+    f1=f1_score(Y_test, predicted, average='binary')
     print(f1)
-    aps=average_precision_score(TCL, predicted)
+
+    aps=average_precision_score(Y_test, predicted)
     print("average_precision_score")
     print(aps)
+
     print("hyperparameters")
     print(model.get_params())
 
-#score()
+score()
