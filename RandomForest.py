fa0d2b2a17d3d94e12d35b73a3a2cf87da27213f
Josh Levy <jdlrm77@gmail.com>
2018-03-07 14:36:38 -0600

    Added Random Forest, confusion matrix

--- RandomForest.py
+++ RandomForest.py
@@ -3,11 +3,12 @@ import numpy as np
 import sklearn
 import pandas as pd
 from scipy.stats import randint, expon 
-from sklearn.svm import SVC
-from sklearn.svm import NuSVC
+#from sklearn.svm import SVC
+#from sklearn.svm import NuSVC
+from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.metrics import accuracy_score, average_precision_score, f1_score
+from sklearn.metrics import accuracy_score, average_precision_score, f1_score, confusion_matrix
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
@@ -44,32 +45,28 @@ print("done")
 X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data)
 
 #Exhaustive grid search parameters
-param_grid = {'C': [1,10, 100, 1000], 'kernel': ['rbf', 'poly', 'sigmoid', 'linear'], 'degree': [0, 1, 2, 3, 4, 5], 'gamma': [1e-3, 1e-4, 1e-5, 1e-6], 'coef0': [0,1,2,3,4,5]}
-#param_grid = {'C': [1,10,100,1000], 'kernel': ['sigmoid'], 'gamma': [1e-3, 1e-4, 1e-5, 1e-6], }, {'C': [1,10,100,1000], 'kernel': ['poly'], 'gamma': [1e-3, 1e-4, 1e-5, 1e-6], 'degree': [1,2,3,4,5,6,7]}, {'C': [1,10,100,1000], 'kernel': ['linear']}
+param_grid = {}
+
 #Randomized grid search parameters
-param_dist = {'C': expon(scale = 100), 
-'gamma': expon(scale = .1), 
-'kernel': ['rbf', 'poly', 'sigmoid', 'linear'], 
-'degree': randint(1,100),
-'coef0': randint(0,100)}
-n_iter_search = 20
+param_dist = {}
+n_estimators = 40
 
 
 #Creating and training model
     #Regular model
 #model = SVC(C= 10, gamma= .0001, kernel= 'rbf', verbose = True)
-SVC = SVC(verbose = False)
+model = RFC(n_estimators, verbose = True)
     #Regular grid search
 #model = GridSearchCV(SVC, param_grid=param_grid)
 #searchtype = GridSearch
     #Randomized Grid search
-model = RandomizedSearchCV(SVC, param_distributions=param_dist, n_iter=n_iter_search)
-searchtype = "Randomized, n_iter_search = {}".format(n_iter_search)
+    #model = RandomizedSearchCV(SVC, param_distributions=param_dist, n_iter=n_iter_search)
+    #searchtype = "Randomized, n_iter_search = {}".format(n_iter_search)
     #Training
 model.fit(X_train, Y_train.values.ravel())
 
     #Cross Validation
-scores = cross_val_score(model, X_data, Y_data.values.ravel(), cv = 5)
+    #scores = cross_val_score(model, X_data, Y_data.values.ravel(), cv = 5)
 
 #finds and prints model scores and statistics
 def score():
@@ -83,11 +80,11 @@ def score():
     accscore=accuracy_score(Y_test, predicted)
     print(accscore)
 
-    print("Cross validation score and 95% CI")
-    CVSmean = scores.mean()
-    print(CVSmean)
-    CI = scores.std()*2
-    print(CI)
+   # print("Cross validation score and 95% CI")
+   # CVSmean = scores.mean()
+   # print(CVSmean)
+   # CI = scores.std()*2
+   # print(CI)
 
     print("f1 score")
     f1=f1_score(Y_test, predicted, average='binary')
@@ -97,16 +94,15 @@ def score():
     print("average_precision_score")
     print(aps)
 
+   # print("model hyperparameters")
+   # params = model.best_params_
+   # print(params)
+
     tn, fp, fn, tp = confusion_matrix(Y_test, predicted).ravel()
-    print("confusion matrix\n tn, fp, fn, tp")
     print(tn, fp, fn, tp)
 
-    print("model hyperparameters")
-    params = model.best_params_
-    print(params)
-
-    save_scores(accscore, CVSmean, CI, f1, aps, params, searchtype)
-
+   # save_scores(accscore, CVSmean, CI, f1, aps, params, searchtype)
+    save_scores(accscore, "NA", "NA", f1, aps, "NA", "NA")
 #uploads scores to google drive
 def save_scores(accscore, CVSmean, CI, f1, aps, params, searchtype):
     json_key = json.load(open('creds.json')) 
